8fcf8078fdc0429319b19db9cce4b7f9aa3d6465
Tory BYU <torys.anderson@gmail.com>
2020-02-11 13:24:50 -0700

    excluding dirs, checking args on sh script

--- rename.sh
+++ rename.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if [ -z "$1" ]
+  then
+      echo "Please provide the new project name."
+      exit 1
+fi
 # Recursively apply given name to template
-$given=$1
-$snake=
-$kebab=
+temp="ZZZZ"
+snake="${1//[ :=+-]/_}"
+kebab="${snake//_/-}"
+echo "Input is: $1"
+echo "Snake is: $snake"
+echo "Kebab is: $kebab"
 # Change text within files
-grep -rl 'ZZZZ' . | xargs sed -i "s@ZZZZ@$kebab@g"
+grep -rl --exclude-dir=.git --exclude rename.sh $temp . | xargs sed -i "s@$temp@$kebab@g"
 # Change filenames appropriately
-find . -depth -iname 'ZZZZ' -exec rename -v 'ZZZZ' $snake {} +
+find . -depth -iname $temp -exec rename -v $temp $snake {} +
