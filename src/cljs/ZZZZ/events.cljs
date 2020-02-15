5dbd32b99b1792794aa5ab9947510167f86cacfd
CambodianCoder <scott.griffin1700@gmail.com>
2020-02-15 08:37:49 -0700

    added http-post and handler-with-http reg-event-fx

--- src/cljs/ZZZZ/events.cljs
+++ src/cljs/ZZZZ/events.cljs
@@ -49,3 +49,40 @@
  (validate-spec ::db/app-db)
  (fn [db [_ m]]
    (assoc db :current-page m)))
+
+(rfc/reg-event-fx
+ :handler-with-http
+ (fn handler-with-http [db [_ {:keys [uri params on-success-kvec]}]]
+   {:http-xhrio {:method          :get
+                 :uri             uri
+             ;    :headers         {"Access-Control-Allow-Origin" "http://localhost:3030"}
+                 :params          params
+                 :timeout         8000
+                 :response-format (ajax/json-response-format {:keywords? true})
+                 :on-success      on-success-kvec
+                 :on-failure      [:on-failure]}}))
+
+(rfc/reg-event-fx
+ :http-post
+ (fn http-post [db [_ {:keys [uri params on-success-kvec]}]]
+   {:http-xhrio {:method          :post
+                 :uri             uri
+                 :params          params
+                 :headers         {"X-CSRF-Token" js/csrfToken}
+                 :timeout         5000
+                 :format          (ajax/json-request-format)
+                 :response-format (ajax/json-response-format {:keywords? true})
+                 :on-success      on-success-kvec
+                 :on-failure      [:on-failure]}}))
+
+(rfc/reg-event-db
+ :on-success
+ (validate-spec ::db/api-result)
+ (fn on-success [db [_ result]]
+   (assoc db :api-result result)))
+
+(rfc/reg-event-db
+ :on-failure
+ (validate-spec ::db/api-error-result)
+ (fn on-failure [db [_ result]]
+   (assoc db :api-error-result result)))
